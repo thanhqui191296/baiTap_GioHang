e442625d2f7c9e0938eadba120bd452d05f783f5
Qui <thanhquy117@gmail.com>
2023-07-18 14:10:34 +0700

    code moi

--- src/assets/bai-tap-gio-hang/baitapgiohang.jsx
+++ src/assets/bai-tap-gio-hang/baitapgiohang.jsx
@@ -3,7 +3,7 @@ import ModalGioHang from './modalgiohang'
 import DanhSachSanPham from './danhsachsanpham'
 import shoeData from '../data/data.json'
 import XemChiTiet from './xemchitiet'
-
+import Modal from './components/modal'
 
 export default class BaiTapGioHang extends Component {
   constructor(props) {
@@ -11,17 +11,12 @@ export default class BaiTapGioHang extends Component {
     this.state = {
       gioHang: [],
       sanPhamChiTiet: null,
-      // {
-      //   id: 1,
-      //   name: "Adidas Prophere",
-      //   alias: "adidas-prophere",
-      //   price: 350,
-      //   description: "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
-      //   shortDescription: "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
-      //   quantity: 995,
-      //   image: "http://svcy3.myclass.vn/images/adidas-prophere.png"
-      // }
-      
+      modal : 
+      {
+        status: "",
+        title: "Xóa Sản Phẩm",
+        content: "Bạn có muốn xóa sản phẩm hay không?"
+      }
     }
   }
 
@@ -43,18 +38,11 @@ export default class BaiTapGioHang extends Component {
     })
   }
   xoaGioHang = (id) => {
-    // var gioHangCapNhat = [...this.state.gioHang]
-    // let index  = gioHangCapNhat.findIndex(sp => sp.id === id)
-    // if (index !== -1){
-    //   gioHangCapNhat.splice(index,1)
-    // }
-    if(window.confirm('Bạn có muốn xóa hay không')){
-      const gioHangCapNhat = this.state.gioHang.filter((sp) => sp.id !== id)
+
+    var gioHangCapNhat = this.state.gioHang.filter(sp => sp.id !== id)
     this.setState({
       gioHang: gioHangCapNhat
     })
-    }
-    
   }
   tangGiamSoLuong = (id, tangGiam) => {
     var gioHangCapNhat = [...this.state.gioHang]
@@ -85,8 +73,8 @@ export default class BaiTapGioHang extends Component {
     return (
       <div className='container'>
         <h3 className='text-center'>ShoeShop</h3>
-        
-        <ModalGioHang tangGiamSoLuong={this.tangGiamSoLuong}  gioHang={this.state.gioHang} xoaGioHang={this.xoaGioHang}/>
+        {/* <Modal modal={this.state.modal} xoaGioHang={this.xoaGioHang}/> */}
+        <ModalGioHang tangGiamSoLuong={this.tangGiamSoLuong} xoaGioHang={this.xoaGioHang}  gioHang={this.state.gioHang}  modal={this.state.modal}/>
         <div className='text-right'><span className='text-danger' style={{ cursor: 'pointer', fontSize: '17px', fontWeight: 'bold' }} data-toggle='modal' data-target='#modelId' >Giỏ hàng ({tongSoLuong})</span></div>
         <DanhSachSanPham themGioHang={this.themGioHang} mangSanPham={shoeData} hanldeChangeSanPham={this.hanldeChangeSanPham}/>
         {this.state.sanPhamChiTiet ? <><XemChiTiet sanPham={this.state.sanPhamChiTiet} /></> : null}
--- src/assets/bai-tap-gio-hang/components/modal.jsx
+++ src/assets/bai-tap-gio-hang/components/modal.jsx
@@ -2,23 +2,52 @@ import React, { Component } from 'react'
 
 export default class Modal extends Component {
     render() {
-        const {onDelete,closeModal,xoaGioHang} = this.props
+        const { modal, xoaGioHang } = this.props
         return (
-            <div className="modal" tabIndex={-1} id='modalXoaSP'>
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Xóa giỏ hàng</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">×</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <p>Bạn có muốn xóa sản phẩm này không?</p>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => onDelete(xoaGioHang)}>Ok</button>
-                            <button type="button" className="btn btn-primary" onClick={() =>closeModal()}>Close</button>
+            <div>
+                <div
+                    className="modal"
+                    tabIndex={-1}
+                    style={{
+                        display: "block",
+                    }}
+                >
+                    <div style={{
+                        position: 'fixed',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)'
+                    }} className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title">{modal.title}</h5>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                />
+                            </div>
+                            <div className="modal-body">
+                                <p>{modal.content}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    onClick={modal.onCancle}
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Không
+                                </button>
+                                <button
+                                    onClick={modal.onOK}
+                                    type="button"
+                                    className="btn btn-primary"
+                                >
+                                    Okk
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
